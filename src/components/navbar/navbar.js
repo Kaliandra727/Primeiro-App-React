d2a3706bca5d110b805dde58cbd50f0f594cbdf6
KALIANDRA DE FREITAS PEREIRA <100894917+Kaliandra727@users.noreply.github.com>
2022-08-18 13:21:07 -0400

    Initial commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./navbar.css";
+
+export default function navbar() {
+  return (
+    <nav className="navbar">
+      <span>React First</span>
+    </nav>
+  );
+}
